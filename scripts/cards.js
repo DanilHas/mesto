1288341c3c5e23dc4fa2b32687c6c5cafad3cf57
Danil <hasanovdan@gmail.com>
2023-03-17 18:28:28 +0300

    fix: сделать рефакторинг кода, исправить замечания после ревью

---
+++ scripts/cards.js
@@ -0,0 +1,32 @@
+const initialCards = [
+  {
+    name: 'Ватикан',
+    link: './images/vatican-museum.jpg',
+    alt: 'Изображение спиральной лестницы в Ватиканском музее',
+  },
+  {
+    name: 'Сидней',
+    link: './images/sydney-opera-house.jpg',
+    alt: 'Изображение оперного театра в Сиднее',
+  },
+  {
+    name: 'Амстердам',
+    link: './images/amsterdam-sluishuis.jpg',
+    alt: 'Изображение необычного жилого здания в Амстердаме',
+  },
+  {
+    name: 'Холмогорский район',
+    link: './images/kholmogorsky-rayon.jpg',
+    alt: 'Изображение путей железной дороги посреди леса',
+  },
+  {
+    name: 'Иваново',
+    link: './images/ivanovo.jpg',
+    alt: 'Вечернее изображение жилых многоквартирных домов. Вид сверху',
+  },
+  {
+    name: 'Архыз',
+    link: './images/arkhyz.jpg',
+    alt: 'Изображение горных вершин в районе Архыза',
+  },
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,59 @@
-const initialCards = [
-  {
-    name: 'Ватикан',
-    link: './images/vatican-museum.jpg',
-    alt: 'Изображение спиральной лестницы в Ватиканском музее',
-  },
-  {
-    name: 'Сидней',
-    link: './images/sydney-opera-house.jpg',
-    alt: 'Изображение оперного театра в Сиднее',
-  },
-  {
-    name: 'Амстердам',
-    link: './images/amsterdam-sluishuis.jpg',
-    alt: 'Изображение необычного жилого здания в Амстердаме',
-  },
-  {
-    name: 'Холмогорский район',
-    link: './images/kholmogorsky-rayon.jpg',
-    alt: 'Изображение путей железной дороги посреди леса',
-  },
-  {
-    name: 'Иваново',
-    link: './images/ivanovo.jpg',
-    alt: 'Вечернее изображение жилых многоквартирных домов. Вид сверху',
-  },
-  {
-    name: 'Архыз',
-    link: './images/arkhyz.jpg',
-    alt: 'Изображение горных вершин в районе Архыза',
-  },
-];
-
-const cardsListElement = document.querySelector('.cards__list');
-const cardTemplate = document.querySelector('#card-template');
+// попапы
+const editProfilePopupElement = document.querySelector(
+  '.popup_type_edit-profile'
+);
+const addCardPopupElement = document.querySelector('.popup_type_add-card');
 const upscalingCardImagePopupElement = document.querySelector(
   '.popup_type_upscaling'
 );
+
+// кнопки на главной странице (открытия попапов)
+const editButtonElement = document.querySelector('.profile__edit-button');
+const addButtonElement = document.querySelector('.profile__add-button');
+
+// кнопки закрытия попапов
+const editProfilePopupCloseButtonElement =
+  editProfilePopupElement.querySelector('.popup__close-button');
+const addCardPopupCloseButtonElement = addCardPopupElement.querySelector(
+  '.popup__close-button'
+);
+const upscalingCardImagePopupCloseButton =
+  upscalingCardImagePopupElement.querySelector('.popup__close-button');
+
+// формы
+const editProfilePopupFormElement =
+  editProfilePopupElement.querySelector('.form');
+const addCardPopupFormElement = addCardPopupElement.querySelector('.form');
+
+// инпуты форм
+const nameInput = editProfilePopupFormElement.querySelector(
+  '.form__input_info_name'
+);
+const jobInput = editProfilePopupFormElement.querySelector(
+  '.form__input_info_job'
+);
+const placeInput = addCardPopupFormElement.querySelector(
+  '.form__input_name_place'
+);
+const imageLinkInput = addCardPopupFormElement.querySelector(
+  '.form__input_name_image-link'
+);
+
+// template
+const cardTemplate = document.querySelector('#card-template');
+
+const cardsListElement = document.querySelector('.cards__list');
+
+const popupCardImage =
+  upscalingCardImagePopupElement.querySelector('.popup__image');
+
+const popupImageCaption = upscalingCardImagePopupElement.querySelector(
+  '.popup__image-caption'
+);
+
+const nameElement = document.querySelector('.profile__name');
+
+const jobElement = document.querySelector('.profile__about-yourself');
 
 // Функция, чтобы поставить/убрать лайк
 const likeCard = (event) => {
@@ -50,9 +68,6 @@ const deleteCard = (event) => {
 
 //Функция увеличения изображения картинки
 const upscalingCardImage = (data) => {
-  const popupCardImage = document.querySelector('.popup__image');
-  const popupImageCaption = document.querySelector('.popup__image-caption');
-
   popupCardImage.src = data.link;
   popupCardImage.alt = data.alt;
   popupImageCaption.textContent = data.name;
@@ -90,24 +105,6 @@ initialCards.forEach((card) => {
   addCard(card, cardsListElement);
 });
 
-// инициализация элементов, необходимых для редактирования профиля
-const editButtonElement = document.querySelector('.profile__edit-button');
-const editProfilePopupElement = document.querySelector(
-  '.popup_type_edit-profile'
-);
-const editProfilePopupCloseButtonElement =
-  editProfilePopupElement.querySelector('.popup__close-button');
-const editProfilePopupFormElement =
-  editProfilePopupElement.querySelector('.form');
-const nameInput = editProfilePopupFormElement.querySelector(
-  '.form__input_info_name'
-);
-const jobInput = editProfilePopupFormElement.querySelector(
-  '.form__input_info_job'
-);
-const nameElement = document.querySelector('.profile__name');
-const jobElement = document.querySelector('.profile__about-yourself');
-
 // функция открытия попапа
 const openPopup = (popup) => {
   popup.classList.add('popup_opened');
@@ -121,38 +118,12 @@ const closePopup = (popup) => {
 // функция отправки формы редактирования профиля
 const handleEditProfileFormSubmit = (event) => {
   event.preventDefault();
+
   nameElement.textContent = nameInput.value;
   jobElement.textContent = jobInput.value;
+
   closePopup(editProfilePopupElement);
 };
-
-// навешивание событий для редактирования профиля
-editButtonElement.addEventListener('click', () => {
-  nameInput.value = nameElement.textContent;
-  jobInput.value = jobElement.textContent;
-  openPopup(editProfilePopupElement);
-});
-editProfilePopupCloseButtonElement.addEventListener('click', () =>
-  closePopup(editProfilePopupElement)
-);
-editProfilePopupFormElement.addEventListener(
-  'submit',
-  handleEditProfileFormSubmit
-);
-
-// инициализация элементов, необходимых для добавления карточки
-const addCardPopupElement = document.querySelector('.popup_type_add-card');
-const addCardPopupCloseButtonElement = addCardPopupElement.querySelector(
-  '.popup__close-button'
-);
-const addButtonElement = document.querySelector('.profile__add-button');
-const addCardPopupFormElement = addCardPopupElement.querySelector('.form');
-const placeInput = addCardPopupFormElement.querySelector(
-  '.form__input_name_place'
-);
-const imageLinkInput = addCardPopupFormElement.querySelector(
-  '.form__input_name_image-link'
-);
 
 // функция отправки формы добавления карточки
 const handleAddCardFormSubmit = (event) => {
@@ -167,24 +138,37 @@ const handleAddCardFormSubmit = (event) => {
   addCard(data, cardsListElement);
   closePopup(addCardPopupElement);
 
-  placeInput.value = '';
-  imageLinkInput.value = '';
+  addCardPopupFormElement.reset();
 };
 
-// события по добавлению карточки
+// обработчики событий для редактирования профиля
+editButtonElement.addEventListener('click', () => {
+  nameInput.value = nameElement.textContent;
+  jobInput.value = jobElement.textContent;
+  openPopup(editProfilePopupElement);
+});
+
+editProfilePopupCloseButtonElement.addEventListener('click', () =>
+  closePopup(editProfilePopupElement)
+);
+
+editProfilePopupFormElement.addEventListener(
+  'submit',
+  handleEditProfileFormSubmit
+);
+
+// обработчики событий по добавлению карточки
 addButtonElement.addEventListener('click', () =>
   openPopup(addCardPopupElement)
 );
+
 addCardPopupCloseButtonElement.addEventListener('click', () =>
   closePopup(addCardPopupElement)
 );
+
 addCardPopupFormElement.addEventListener('submit', handleAddCardFormSubmit);
 
-// инициализация кнопки закрытия попапа увеличения изображения картинки
-const upscalingCardImagePopupCloseButton =
-  upscalingCardImagePopupElement.querySelector('.popup__close-button');
-
-// событие закрытия попапа увеличения изображения картинки
+// обработчик события закрытия попапа увеличения изображения картинки
 upscalingCardImagePopupCloseButton.addEventListener('click', () =>
   closePopup(upscalingCardImagePopupElement)
 );
